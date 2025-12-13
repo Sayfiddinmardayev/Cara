194229617604b982300a0eeb50cc480903c9be52
Sayfiddinmardayev <Sayfiddinmardayev@gmail.com>
2025-12-14 00:18:02 +0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Script for navigation bar
+let navbarList = document.querySelector(".navbar-list")
+let menuBtn = document.querySelector(".menu")
+menuBtn.addEventListener("click", () => {
+    navbarList.classList.toggle("active")
+})
